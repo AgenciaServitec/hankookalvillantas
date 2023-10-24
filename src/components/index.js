59b3d98c176c511c5346bb43832ef6519f2a4c0c
servitecperu266 <servitecperu266@gmail.com>
2023-10-24 18:57:57 -0500

    add footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Card } from "./Card.astro";
 export { default as Button } from "./Button.astro";
+export { default as IconItem } from "./ui/IconItem.astro";
 export { Form } from "./Form.jsx";
