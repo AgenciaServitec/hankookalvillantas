7dc92b99f1cc98eb87b971b29185230c6bfc87fd
noelMoriano <nmoriano26@gmail.com>
2023-10-24 02:17:48 -0500

    initial astro project

---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// interface Props {
+//     onSubmit?: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
+//     children?: React.ReactNode;
+// }
+
+export const Form = ({ children, ...props }) => (
+  <form noValidate autoComplete="off" {...props}>
+    <div
+      className="form-content"
+      style={{ display: "flex", flexDirection: "column", gap: "1em" }}
+    >
+      {children}
+    </div>
+  </form>
+);
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Card } from "./Card.astro";
+export { default as Button } from "./Button.astro";
+export { Form } from "./Form.jsx";
