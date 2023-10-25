93d06602b5897a38d75b059d42fd8bcaa64b9b7e
servitecperu266 <servitecperu266@gmail.com>
2023-10-25 17:51:06 -0500

    update responsive design

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Card } from "./Card.astro";
 export { default as Button } from "./Button.astro";
 export { default as IconItem } from "./ui/IconItem.astro";
+export { default as IconFontAwesome } from "./ui/IconFontAwesome.astro";
 export { Form } from "./Form.jsx";
