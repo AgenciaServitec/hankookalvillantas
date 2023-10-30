d5ee18fea8493224aa6c135a3dbb9d454810b6d2
noelMoriano <nmoriano26@gmail.com>
2023-10-30 10:23:09 -0500

    Refactoring files

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,6 +1,11 @@
+//React components
+export { Form } from "./Form.jsx";
+
+//Astro components
 export { default as Button } from "./Button.astro";
 export { default as IconItem } from "./IconItem.astro";
 export { default as IconFontAwesome } from "./IconFontAwesome.astro";
 export { default as Title } from "./Title.astro";
 export { default as CardProduct } from "./CardProduct.astro";
-export { Form } from "./Form.jsx";
+export { default as SectionWrapper } from "./SectionWrapper.astro";
+export { default as Divider } from "./Divider.astro";
