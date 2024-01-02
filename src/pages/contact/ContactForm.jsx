bbe60edab919c0b9d325675c7a6897ecc5005c0a
angelgamonal <angelgamonal.t@gmail.com>
2024-01-02 18:40:51 -0500

    fix formData

--- src/pages/contact/ContactForm.jsx
+++ src/pages/contact/ContactForm.jsx
@@ -1,13 +1,56 @@
-import { ButtonJsx, Form, Input, Textarea } from "../../components/index.js";
+import { ButtonJsx, Form, Input, Textarea } from "../../components/ui";
+import { useState } from "react";
 
+const initialForm = {
+  firstName: "",
+  email: "",
+  phoneNumber: "",
+  message: "",
+};
 const ContactForm = () => {
+  const [formData, setFormData] = useState(initialForm);
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
   return (
-    <Form>
-      <Input placeholder="Ingrese Nombres" />
-      <Input type="email" placeholder="Ingrese email" />
-      <Input type="numbre" placeholder="Ingrese teléfono" />
-      <Textarea />
-      <ButtonJsx>Enviar</ButtonJsx>
+    <Form onSubmit={handleSubmit} method="POST">
+      <Input
+        type="text"
+        name="firstName"
+        value={formData.firstName}
+        placeholder="Ingrese Nombres"
+        onChange={handleChange}
+      />
+      <Input
+        type="mail"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        placeholder="Ingrese email"
+      />
+      <Input
+        type="number"
+        name="phoneNumber"
+        value={formData.phoneNumber}
+        onChange={handleChange}
+        placeholder="Ingrese teléfono"
+      />
+      <Textarea
+        name="message"
+        value={formData.message}
+        onChange={handleChange}
+      />
+
+      <ButtonJsx type="submit">Enviar</ButtonJsx>
     </Form>
   );
 };
