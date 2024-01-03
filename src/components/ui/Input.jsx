75aaef893b4273bdef3d4f3376d80e1e8359b74e
angelgamonal <angelgamonal.t@gmail.com>
2024-01-03 18:52:45 -0500

    refactor props

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,23 +1,44 @@
 export const Input = ({
   type = "text",
   placeholder = "Escribe tu nombre",
-  ...props
+  value = "",
+  name,
+  onChange,
+  error = false,
+  errorMsg = "",
 }) => (
   <>
-    <input
-      type={type}
-      placeholder={placeholder}
-      style={{
-        padding: "0.8em 0.5em",
-        borderRadius: "0.5em",
-        maxWidth: "100%",
-        minWidth: "100%",
-        border: "none",
-        outline: "none",
-        fontSize: "1em",
-        fontFamily: "system-ui, sans-serif",
-      }}
-      {...props}
-    />
+    <label htmlFor="" style={{ maxWidth: "100%", minWidth: "100%" }}>
+      <input
+        type={type}
+        placeholder={placeholder}
+        style={{
+          padding: "0.8em 0.5em",
+          borderRadius: "0.5em",
+          maxWidth: "100%",
+          minWidth: "100%",
+          border: error ? "2px solid red" : "none",
+          outline: "none",
+          fontSize: "1em",
+          fontFamily: "system-ui, sans-serif",
+        }}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+      {error && (
+        <p
+          style={{
+            color: "red",
+            fontWeight: 500,
+            transition: " .2s ease-out",
+            textAlign: "start",
+            padding: "0.5em 0 0 0.5em",
+          }}
+        >
+          {errorMsg}
+        </p>
+      )}
+    </label>
   </>
 );
--- src/components/ui/Textarea.jsx
+++ src/components/ui/Textarea.jsx
@@ -1,19 +1,44 @@
-export const Textarea = ({ placeholder = "Escribe mensaje", ...props }) => {
+export const Textarea = ({
+  placeholder = "Escribe mensaje",
+  value = "",
+  name,
+  onChange,
+  error = false,
+  errorMsg = "",
+}) => {
   return (
-    <textarea
-      placeholder={placeholder}
-      style={{
-        padding: "0.8em 0.5em",
-        maxWidth: "100%",
-        minWidth: "100%",
-        borderRadius: "0.5em",
-        border: "none",
-        outline: "none",
-        fontSize: "1em",
-        minHeight: "5em",
-        fontFamily: "system-ui, sans-serif",
-      }}
-      {...props}
-    ></textarea>
+    <>
+      <label htmlFor="" style={{ maxWidth: "100%", minWidth: "100%" }}>
+        <textarea
+          placeholder={placeholder}
+          style={{
+            padding: "0.8em 0.5em",
+            maxWidth: "100%",
+            minWidth: "100%",
+            borderRadius: "0.5em",
+            border: error ? "2px solid red" : "none",
+            outline: "none",
+            fontSize: "1em",
+            minHeight: "5em",
+            fontFamily: "system-ui, sans-serif",
+          }}
+          name={name}
+          onChange={onChange}
+          value={value}
+        ></textarea>
+        {error && (
+          <p
+            style={{
+              color: "red",
+              fontWeight: 500,
+              textAlign: "start",
+              padding: "0.5em 0 0 0.5em",
+            }}
+          >
+            {errorMsg}
+          </p>
+        )}
+      </label>
+    </>
   );
 };
